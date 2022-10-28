6b5699303627c1ab4f749064640a67979db7a711
Abbes <abbes.boulebtina@gmail.com>
2022-10-28 17:52:54 -0400

    TPs

--- tps/tp03.js
+++ tps/tp03.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path')
 const filePath = path.join(__dirname,'files','rawFile.txt')
-const data = fs.readFileSync(filePath) // blocks here until file is read
-console.log(data)
-moreWork() // will run after readFileSync
+fs.readFile(filePath, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }); 
+
+  moreWork(); // will run before console.log inside readFile
 
 function moreWork(){
     console.log("More work")
