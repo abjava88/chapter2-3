8cf0e5393d18cab9fa6d55eb8c3a8b76ce1bb39e
Abbes <abbes.boulebtina@gmail.com>
2022-10-29 17:38:16 -0400

    Add modifications

--- tps/tp01.js
+++ tps/tp01.js
@@ -2,11 +2,11 @@ function terminator(someInput, callback) {
     callback(`${someInput} and terminated by executing callback `);
   }
   
-  function middleware(someInput, callback) {
+function middleware(someInput, callback) {
     return terminator(`${someInput} touched by middleware `, callback);
   }
   
-  function initiate() {
+function initiate() {
     const someInput = 'hello this is a function ';
     middleware(someInput, function (result) {
       console.log(result);
